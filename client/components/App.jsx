bd1a7660553a3ab88b1d2022012019ca85c6071b
Jared Lewis <jareddlewis@gmail.com>
2021-08-13 20:10:13 -0500

    boilerplate

---
+++ client/components/App.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+// import React, { useState } from 'react';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+  render() {
+    return (
+      <div>
+        <div>
+          Literally Anything Will Make Me Happy
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
