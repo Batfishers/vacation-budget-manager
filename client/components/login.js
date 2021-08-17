8a7439dcf56002a6d0610be87d4f34eb1dc2ca34
eric-olaya <ericolaya@gmail.com>
2021-08-16 21:13:01 -0400

    added database and syncs user with database

--- client/components/login.js
+++ client/components/login.js
@@ -3,8 +3,34 @@ import { TextField, Box, Paper } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 
 const Login = () => {
+    const loginClick = () => {
+        const username = document.getElementById('usernameBox').value;
+        const password = document.getElementById('passwordBox').value;
+        const user = {
+            username: username,
+            password: password
+        }
+        console.log('user obj is: ', user)
+        fetch(`/login`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+          })
+            .then((response) => response.json())
+            .then((response) => {
+              if (typeof response === 'string') console.log(response);
+              else if (typeof response === 'object') console.log('successfully logged in');
+            })
+            .catch((err) => {
+              console.log(err);
+            })
+    }
+    const signupClick = () => {
+        
+    }
 return (
-    <div>
 	<div id='loginPage'>
 	<h2>Login</h2>
     <div id='loginBox'>
@@ -12,12 +38,15 @@ return (
             Username: <TextField id='usernameBox' label='username' variant='filled'></TextField>
             Password:<TextField id='passwordBox' label='password' variant='filled' type='password'></TextField>
         </div>
-        <div id='loginButton'>
-        <Button variant='contained' color='primary'>Login</Button>
-        </div>
+    <div id='loginButton'>
+        <Button variant='contained' color='primary' onClick={loginClick}>Login</Button>
+    </div>
+    <p id='noAccount'>Don't Have an account? Sign in below: </p>
+    <div className='signinButton'>
+    <Button variant='contained' color='primary' onClick={signupClick}>Sign up!</Button>
+    </div>
     </div>
 	</div>
-    </div>
 );
 };
 
--- server/server.js
+++ server/server.js
@@ -2,11 +2,12 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const models = require("./models/vacationBudgetModels.js");
+const userController = require('./controllers/userController');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post("/testdb", (req, res) => {
+app.post('/testdb', (req, res) => {
   models.Test.create(req.body)
     .then(() => {
       return res.status(200).send('Added to DB. Check cluster')
@@ -14,6 +15,22 @@ app.post("/testdb", (req, res) => {
     .catch((err) => {
       return res.status(400).send('Error in testdb' + JSON.stringify(err));
     })
+});
+
+app.post('/signup', userController.createUser, (req, res) => {
+  return res.status(200).json({success: true});
+});
+
+app.post('/login', userController.verifyUser, (req, res) => {
+  let responseToClient;
+  if (res.locals.verifyResponse) {
+    responseToClient = res.locals.verifyResponse;
+  } else {
+    responseToClient = {
+      success: true
+    };
+  }
+  return res.status(200).json(responseToClient);
 });
 
 app.use("/build", express.static(path.join(__dirname, "../build")));
